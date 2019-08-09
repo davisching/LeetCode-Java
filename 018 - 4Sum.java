a84e07092dec17a02902320ea080a91e50e7caba
景诗维 <davisching@outlook.com>
2019-08-09 16:19:16 +0800

    Update 018 - 4Sum.java

--- 018 - 4Sum.java
+++ 018 - 4Sum.java
@@ -1,6 +1,7 @@
 /*  018 - 4Sum - From @ LeetCode.com
     
-    Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+    Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums 
+    such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
 
     Note:
     The solution set must not contain duplicate quadruplets.
