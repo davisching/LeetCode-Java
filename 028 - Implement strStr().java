d5a6d3953083c1aa264551a55bd0d1ee23c76d44
景詩維 <davisching@outlook.com>
2019-08-12 17:55:29 +0800

    Create 028 - Implement strStr().java

---
+++ 028 - Implement strStr().java
@@ -0,0 +1,33 @@
+/*  026 - Remove Duplicates from Sorted Array - From @ LeetCode.com
+    
+    Implement strStr().
+    Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+    Example 1:
+    Input: haystack = "hello", needle = "ll"
+    Output: 2
+
+    Example 2:
+    Input: haystack = "aaaaa", needle = "bba"
+    Output: -1
+
+    Clarification:
+    What should we return when needle is an empty string? This is a great question to ask during an interview.
+    For the purpose of this problem, we will return 0 when needle is an empty string. 
+    This is consistent to C's strstr() and Java's indexOf().
+
+    Solution by @ Davis Ching, 2019
+ */
+ 
+ class Solution {
+    public int strStr(String haystack, String needle) {
+        loop:
+        for (int i = 0; ; i++)
+            for (int j = 0; ; j++) {
+                if (j == needle.length()) return i;
+                if (j + i == haystack.length()) break loop;
+                if (haystack.charAt(i+j) != needle.charAt(j)) break;
+            }
+        return -1;
+    }
+}
