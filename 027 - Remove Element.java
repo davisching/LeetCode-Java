c8056daa95f33bebd2dffcefe7ff0d7c00d2e9eb
景詩維 <davisching@outlook.com>
2019-08-13 15:52:17 +0800

    Update 027 - Remove Element.java

--- 027 - Remove Element.java
+++ 027 - Remove Element.java
@@ -1,4 +1,4 @@
-/*  026 - Remove Duplicates from Sorted Array - From @ LeetCode.com
+/*  027 - Remove Element - From @ LeetCode.com
     
     Given an array nums and a value val, remove all instances of that value in-place and return the new length.
     Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
