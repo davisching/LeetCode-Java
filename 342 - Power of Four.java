b8274acc699ae170c6e3e11975f562f621cfb243
景诗维 <davisching@outlook.com>
2019-12-23 14:42:06 +0800

    Create 342 - Power of Four.java

---
+++ 342 - Power of Four.java
@@ -0,0 +1,24 @@
+/*  342 - Power of Four - From @ LeetCode.com
+    
+    Given an integer (signed 32 bits), write a function to check whether it is a power of 4.
+
+    Example 1:
+
+    Input: 16
+    Output: true
+
+    Example 2:
+
+    Input: 5
+    Output: false
+
+    Follow up: Could you solve it without loops/recursion?
+    
+    Solution by @ Davis Ching, 2019
+ */
+ 
+ class Solution {
+    public boolean isPowerOfFour(int num) {
+        return java.util.regex.Pattern.matches("1(00)*", Integer.toBinaryString(num));
+    }
+}
