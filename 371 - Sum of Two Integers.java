60aab4ecc52f4172f3b10716a14088b364c8bce3
景诗维 <davisching@outlook.com>
2019-12-30 16:51:12 +0800

    Create 371 - Sum of Two Integers.java

---
+++ 371 - Sum of Two Integers.java
@@ -0,0 +1,22 @@
+/*  371 - Sum of Two Integers - From @ LeetCode.com
+    
+    Calculate the sum of two integers a and b, but you are not allowed to use the operator + and -.
+
+    Example 1:
+
+    Input: a = 1, b = 2
+    Output: 3
+
+    Example 2:
+
+    Input: a = -2, b = 3
+    Output: 1
+    
+    Solution by @ Davis Ching, 2019
+ */
+
+class Solution {
+    public int getSum(int a, int b) {
+        return b == 0 ? a : getSum(a^b, (a&b)<<1);
+    }
+}
